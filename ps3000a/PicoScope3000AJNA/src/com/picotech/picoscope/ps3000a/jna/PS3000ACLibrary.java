3c621d574ac11dafd5f4034644510ca7b0fefec4
uia38211 <balazs.papp@continental-corporation.com>
2020-05-17 07:46:30 +0200

    Trigger methods defined

--- ps3000a/PicoScope3000AJNA/src/com/picotech/picoscope/ps3000a/jna/PS3000ACLibrary.java
+++ ps3000a/PicoScope3000AJNA/src/com/picotech/picoscope/ps3000a/jna/PS3000ACLibrary.java
@@ -18,99 +18,131 @@ package com.picotech.picoscope.ps3000a.jna;
 import com.sun.jna.Library;
 import com.sun.jna.Native;
 import com.sun.jna.Pointer;
+import com.sun.jna.Structure;
+import com.sun.jna.Structure.*;
 import com.sun.jna.ptr.FloatByReference;
 import com.sun.jna.ptr.IntByReference;
 import com.sun.jna.ptr.ShortByReference;
 
-public interface PS3000ACLibrary extends Library
-{
-	
-    PS3000ACLibrary INSTANCE = (PS3000ACLibrary) Native.loadLibrary
-                                ("ps3000a", PS3000ACLibrary.class
-                                );
+public interface PS3000ACLibrary extends Library {
 
-    // Method definitions from ps3000aApi.h C header file
-    // ==================================================
-    
-    // C prototype definition : 
-    // 		uint32_t ps3000aEnumerateUnits(int16_t * count, int8_t * serials, int16_t serialLth)
-    int ps3000aEnumerateUnits(ShortByReference count, byte[] serials, ShortByReference serialLth);
+	PS3000ACLibrary INSTANCE = (PS3000ACLibrary) Native.loadLibrary("ps3000a", PS3000ACLibrary.class);
 
-    // C prototype definition :	
-    int ps3000aOpenUnit(ShortByReference handle, String serial);
+	// Method definitions from ps3000aApi.h C header file
+	// ==================================================
 
-    // C prototype definition
-    // uint32_t ps3000aCloseUnit(int16_t handle);
-    int ps3000aCloseUnit(short handle);
+	// C prototype definition :
+	// uint32_t ps3000aEnumerateUnits(int16_t * count, int8_t * serials, int16_t
+	// serialLth)
+	int ps3000aEnumerateUnits(ShortByReference count, byte[] serials, ShortByReference serialLth);
 
-    //uint32_t ps3000aGetUnitInfo(int16_t      handle, int8_t      *string, int16_t      stringLength,
-    //int16_t     *requiredSize, PICO_INFO  info)
-    int ps3000aGetUnitInfo(short handle, byte[] string, short stringLength, ShortByReference requiredSize, int info);
+	// C prototype definition :
+	int ps3000aOpenUnit(ShortByReference handle, String serial);
 
-    // C prototype definition
-    //uint32_t ps3000aChangePowerSource(int16_t      handle, PICO_STATUS powerstate)
-    int ps3000aChangePowerSource(short handle, int powerstate);
+	// C prototype definition
+	// uint32_t ps3000aCloseUnit(int16_t handle);
+	int ps3000aCloseUnit(short handle);
 
-    // uint32_t ps3000aSetChannel(int16_t handle, PS3000A_CHANNEL channel, short enabled, PS3000A_COUPLING type, PS3000A_RANGE range, float analogueOffset)
-    int ps3000aSetChannel(short handle, int channel, short enabled, int type, int range, float analogueOffset);
-     
-    // uint32_t ps3000aGetTimebase2(int16_t handle, uint32_t timebase, int32_t noSamples, float *timeIntervalNanoseconds, int16_t oversample, int32_t *maxSamples, uint32_t segmentIndex)
-    int ps3000aGetTimebase2(short handle, int timebase, int noSamples, FloatByReference timeIntervalNanoseconds, short oversample, IntByReference maxSamples, int segmentIndex);
-    
-    // uint32_t ps3000aSetDataBuffer(int16_t handle, PS3000A_CHANNEL channelOrPort, int16_t *buffer, int32_t bufferLth, uint32_t segmentIndex, PS3000A_RATIO_MODE  mode)
-    int ps3000aSetDataBuffer(short handle, int channelOrPort, Pointer buffer, int bufferLength, int segmentIndex, int ratioMode);
-    
-    
-    // Enumerations
-    // ============
+	// uint32_t ps3000aGetUnitInfo(int16_t handle, int8_t *string, int16_t
+	// stringLength,
+	// int16_t *requiredSize, PICO_INFO info)
+	int ps3000aGetUnitInfo(short handle, byte[] string, short stringLength, ShortByReference requiredSize, int info);
 
-    public enum PS3000AChannel
-    {
-        PS3000A_CHANNEL_A,
-        PS3000A_CHANNEL_B,
-        PS3000A_CHANNEL_C,
-        PS3000A_CHANNEL_D,
-        PS3000A_EXTERNAL
-    }
-    
-    public enum PS3000ACoupling
-    {
-        PS3000A_AC,
-        PS3000A_DC
-    }
-    
-    public enum PS3000ARange
-    {
-        PS3000A_10MV,
-        PS3000A_20MV,
-        PS3000A_50MV,
-        PS3000A_100MV,
-        PS3000A_200MV,
-        PS3000A_500MV,
-        PS3000A_1V,
-        PS3000A_2V,
-        PS3000A_5V,
-        PS3000A_10V,
-        PS3000A_20V,
-        PS3000A_50V,
-        PS3000A_MAX_RANGES
-    }
-    
-    public enum PS3000ARatioMode
-    {
-        PS3000A_RATIO_MODE_NONE(0),
-        PS3000A_RATIO_MODE_AGGREGATE(1),
-        PS3000A_RATIO_MODE_DECIMATE(2),
-        PS3000A_RATIO_MODE_AVERAGE(4);
-        
-        private final int ratio;
-        
-        PS3000ARatioMode(int ratio)
-        {
-            this.ratio = ratio;
-        }
-    }
-    
+	// C prototype definition
+	// uint32_t ps3000aChangePowerSource(int16_t handle, PICO_STATUS powerstate)
+	int ps3000aChangePowerSource(short handle, int powerstate);
+
+	// uint32_t ps3000aSetChannel(int16_t handle, PS3000A_CHANNEL channel, short
+	// enabled, PS3000A_COUPLING type, PS3000A_RANGE range, float analogueOffset)
+	int ps3000aSetChannel(short handle, int channel, short enabled, int type, int range, float analogueOffset);
+
+	// uint32_t ps3000aGetTimebase2(int16_t handle, uint32_t timebase, int32_t
+	// noSamples, float *timeIntervalNanoseconds, int16_t oversample, int32_t
+	// *maxSamples, uint32_t segmentIndex)
+	int ps3000aGetTimebase2(short handle, int timebase, int noSamples, FloatByReference timeIntervalNanoseconds,
+			short oversample, IntByReference maxSamples, int segmentIndex);
+
+	// uint32_t ps3000aSetDataBuffer(int16_t handle, PS3000A_CHANNEL channelOrPort,
+	// int16_t *buffer, int32_t bufferLth, uint32_t segmentIndex, PS3000A_RATIO_MODE
+	// mode)
+	int ps3000aSetDataBuffer(short handle, int channelOrPort, Pointer buffer, int bufferLength, int segmentIndex,
+			int ratioMode);
+
+	/*
+	 * ========================================================= Functions missing
+	 * from the original (minimalist) example
+	 * =========================================================
+	 */
+
+	/*
+	 * 
+	 * PREF0 PREF1 PICO_STATUS PREF2 PREF3 (ps3000aSetTriggerChannelProperties) (
+	 * int16_t handle, PS3000A_TRIGGER_CHANNEL_PROPERTIES *channelProperties,
+	 * int16_t nChannelProperties, int16_t auxOutputEnable, int32_t
+	 * autoTriggerMilliseconds );
+	 * 
+	 */
+
+	int ps3000aSetTriggerChannelProperties(short handle, PS3000A_TRIGGER_CHANNEL_PROPERTIES channelProperties,
+			short nChannelProperties, short auxOutputEnable, int autoTriggerMilliseconds);
+
+	// Enumerations
+	// ============
+
+	public enum PS3000AChannel {
+		PS3000A_CHANNEL_A, PS3000A_CHANNEL_B, PS3000A_CHANNEL_C, PS3000A_CHANNEL_D, PS3000A_EXTERNAL
+	}
+
+	public enum PS3000ACoupling {
+		PS3000A_AC, PS3000A_DC
+	}
+
+	public enum PS3000ARange {
+		PS3000A_10MV, PS3000A_20MV, PS3000A_50MV, PS3000A_100MV, PS3000A_200MV, PS3000A_500MV, PS3000A_1V, PS3000A_2V,
+		PS3000A_5V, PS3000A_10V, PS3000A_20V, PS3000A_50V, PS3000A_MAX_RANGES
+	}
+
+	public enum PS3000ARatioMode {
+		PS3000A_RATIO_MODE_NONE(0), PS3000A_RATIO_MODE_AGGREGATE(1), PS3000A_RATIO_MODE_DECIMATE(2),
+		PS3000A_RATIO_MODE_AVERAGE(4);
+
+		private final int ratio;
+
+		PS3000ARatioMode(int ratio) {
+			this.ratio = ratio;
+		}
+	}
+
+	public enum PS3000A_CHANNEL {
+		  PS3000A_CHANNEL_A,
+		  PS3000A_CHANNEL_B,
+		  PS3000A_CHANNEL_C,
+		  PS3000A_CHANNEL_D,
+		  PS3000A_EXTERNAL,
+		  PS3000A_TRIGGER_AUX,
+		  PS3000A_MAX_TRIGGER_SOURCES
+	}
+
+	public enum PS3000A_THRESHOLD_MODE {
+		PS3000A_LEVEL, PS3000A_WINDOW
+	}
+
+	// Structures
+	// ==========
+
+	/*
+	 * typedef struct tPS3000ATriggerChannelProperties { int16_t thresholdUpper;
+	 * uint16_t thresholdUpperHysteresis; int16_t thresholdLower; uint16_t
+	 * thresholdLowerHysteresis; PS3000A_CHANNEL channel; PS3000A_THRESHOLD_MODE
+	 * thresholdMode; } PS3000A_TRIGGER_CHANNEL_PROPERTIES;
+	 */
+	@FieldOrder({"thresholdUpper", "thresholdUpperHysteresis", "thresholdLower", "thresholdLowerHysteresis", "channel", "thresholdMode"})
+	public static class PS3000A_TRIGGER_CHANNEL_PROPERTIES extends Structure {
+		public short thresholdUpper;
+		public short thresholdUpperHysteresis;
+		public short thresholdLower;
+		public short thresholdLowerHysteresis;
+		public int channel;
+		public int thresholdMode;
+	}
 }
-
-   
